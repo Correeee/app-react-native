2835f510b2fcffa9542f5007f6705d7e1c33b280
elcorre <correa755@hotmail.com>
2023-04-18 17:43:45 -0300

    Item list

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
+import { styles } from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>¡Hola Coder! Soy Maximiliano Correa.</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container__div}>
+        <TextInput placeholder='Input' style={styles.container__div_input}></TextInput>
+        <Button title='Add' color={'#e63946'} />
+      </View>
+      <View style={styles.listcontainer}>
+        <View style={styles.itemcontainer}>
+          <Text style={styles.item}>ITEM 1</Text>
+        </View>
+        <View style={styles.itemcontainer}>
+          <Text style={styles.item}>ITEM 2</Text>
+        </View>
+        <View style={styles.itemcontainer}>
+          <Text style={styles.item}>ITEM 3</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
